2becb407f2624e57c45942702c6f6086406fbb28
dt_riya <riya.juturuchinnareddy@dispatchtrack.com>
2024-07-16 19:02:22 +0530

    Week-5 Task-6, extract matches table data from given csv file

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,19 @@
+require 'csv'
+
 class Match < ApplicationRecord
+
+  def self.populate_from_csv(file_path)
+    CSV.foreach(file_path, headers: true) do |row|
+      Match.create!(
+        date: row['date'],
+        location: row['location'],
+        team1_id: row['team1_id'],
+        team2_id: row['team2_id'],
+        score_team1: row['score_team1'],
+        score_team2: row['score_team2']
+      )
+    end
+  rescue => e
+    puts "An error occurred: #{e.message}"
+  end
 end
